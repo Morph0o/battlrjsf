6eabd73f1b4f8c8f515b2b6a7bea1f14b858e936
Mickey <detrodoniv@yahoo.com>
2020-05-26 12:38:34 -0400

    add mob page with mobs

--- home.js
+++ home.js
@@ -1,12 +1,88 @@
 document.addEventListener('DOMContentLoaded',()=>{
     console.log("dom loaded")
-    addTitle()
+    fightPage()
+    mobsPage()
+    monstPage()
+    aboutPage()
 
 })
 
-function addTitle(){
-    let div = document.querySelector("#root")
-let message = document.createElement("h1")
-message.innerText = "Welcome to BattlrJS"
-div.appendChild(message)
+
+
+function fightPage(){
+    var ftbtn = document.querySelector("#fightbtn")
+    ftbtn.addEventListener('click',loadFight)
+}
+
+function loadFight(){
+    fetch()
+    var page = document.querySelector("#page")
+    page.innerHTML = ""
+    var fth1 = document.createElement("h1")
+    fth1.innerText = "Fight"
+    page.appendChild(fth1)
+}
+function mobsPage(){
+    var ftbtn = document.querySelector("#mobsbtn")
+    ftbtn.addEventListener('click',loadMobs)
+}
+
+function loadMobs(){
+    var page = document.querySelector("#page")
+    page.innerHTML = ""
+    var fth2 = document.createElement("h2")
+    fth2.innerText = "Playable Mobs"
+    fth2.classList.add("text-center")
+    page.appendChild(fth2)
+    fetch("http://localhost:3000/mobs")
+    .then(response => response.json())
+    .then(mobArray => {
+        mobArray.forEach(mob => {renderMob(mob)})
+    })
+
+}
+function renderMob(mob){
+    console.log(mob)
+   var page = document.querySelector("#page")
+
+   var mobdiv = document.createElement("div")
+   mobdiv.classList.add("card","center")
+
+   var mobname = document.createElement("h3")
+   mobname.innerText = mob.name
+   mobdiv.appendChild(mobname)
+
+   var mobimg = document.createElement("img")
+   mobimg.src = mob.img
+   mobimg.classList.add("image")
+   mobdiv.appendChild(mobimg)
+
+   page.appendChild(mobdiv)
+   
+}
+
+function monstPage(){
+    var ftbtn = document.querySelector("#monstbtn")
+    ftbtn.addEventListener('click',loadMonst)
+}
+
+function loadMonst(){
+    var page = document.querySelector("#page")
+    page.innerHTML = ""
+    var fth1 = document.createElement("h1")
+    fth1.innerText = "Monst"
+    page.appendChild(fth1)
+}
+
+function aboutPage(){
+    var ftbtn = document.querySelector("#aboutbtn")
+    ftbtn.addEventListener('click',loadAbout)
+}
+
+function loadAbout(){
+    var page = document.querySelector("#page")
+    page.innerHTML = ""
+    var ftp = document.createElement("p")
+    ftp.innerText = "This is a Text based Fighting game. Created with Rails and Vanilla JS"
+    page.appendChild(ftp)
 }
