7731ee2c1ae5278460f9eb1385c8797b3f627551
Mickey <detrodoniv@yahoo.com>
2020-06-05 11:34:43 -0400

    updated fight page

--- home.js
+++ home.js
@@ -15,12 +15,63 @@ function fightPage(){
 }
 
 function loadFight(){
-    fetch()
+    
     var page = document.querySelector("#page")
     page.innerHTML = ""
     var fth1 = document.createElement("h1")
     fth1.innerText = "Fight"
     page.appendChild(fth1)
+    mobDiv = document.createElement("div")
+    mobDiv.id = "mobDiv"
+    page.appendChild(mobDiv)
+    mobDiv.classList.add("float-left")
+    selectDiv = document.createElement("div")
+    selectDiv.classList.add("float-left")
+    mobSelect = document.createElement("select")
+    mobSelect.id = "mobSelect"
+    mobSelect.addEventListener("change",renderSelectMob)
+    page.appendChild(selectDiv)
+    dummyOption = document.createElement("option")
+    dummyOption.innerText = "Select Mob"
+    dummyOption.value = ""
+    dummyOption.disable = true
+    mobSelect.appendChild(dummyOption)
+    selectDiv.appendChild(mobSelect)
+    fetch("http://localhost:3000/mobs")
+    .then(response => response.json())
+    .then(mobArray => {
+        mobArray.forEach(mob => {renderMobSelect(mob)})
+    })
+
+}
+function renderSelectMob(event){
+   let mobDiv = document.querySelector("#mobDiv")
+   console.log(mobDiv)
+    mobDiv.innerHTML = "" 
+    fetch(`http://localhost:3000/mobs/${event.target.value}`)
+    .then(response=>response.json())
+    .then(mob=> renderMob(mob))
+
+
+}
+
+function renderMob(mob){
+   let mobDiv = document.querySelector("mobDiv")
+    mobImg = document.createElement("img")
+    mobImg.alt = "mob"
+    mobImg.src =mob.img
+    mobDiv.appendChild(mobImg)
+
+}
+
+function renderMobSelect(mob){
+    mobSelect = document.querySelector("#mobSelect")
+    mobOption = document.createElement("option")
+    mobOption.id = `mob-${mob.id}`
+    mobOption.innerText = `${mob.name}`
+    mobOption.value = `${mob.id}`
+    mobSelect.appendChild(mobOption)
+
 }
 function mobsPage(){
     var ftbtn = document.querySelector("#mobsbtn")
